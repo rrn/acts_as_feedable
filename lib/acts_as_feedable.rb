bc804d87fc5480731cc569281ed7f88eccb048e8
Ryan Wallace <rywall@gmail.com>
2012-04-30 14:16:17 -0700

    copy-paste.

--- lib/acts_as_feedable.rb
+++ lib/acts_as_feedable.rb
@@ -2,7 +2,7 @@ require 'feedable/acts_as_feedable'
 
 module ActsAsFeedable
   class Engine < Rails::Engine
-    initializer "glint.init" do
+    initializer "acts_as_feedable.init" do
       ActiveRecord::Base.send :extend, Feedable::ActsAsFeedable::ActMethod
     end    
   end
