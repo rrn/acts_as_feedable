373e3c8cb7bcb8b911b61493151b126e183a0d2c
Ryan Wallace <rywall@gmail.com>
2012-05-24 15:55:13 -0700

    Add without_unjoined.

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -10,6 +10,13 @@ class Feed < ActiveRecord::Base
   acts_as_joinable_component :parent => 'permission_inheritance_target', :polymorphic => true
   
   default_scope order('feeds.updated_at DESC')
+
+  # Filter feeds about public joinables that you haven't joined
+  scope :without_unjoined, lambda {|joinable_type, user| 
+    where("feeds.scoping_object_type IS NULL OR 
+           feeds.scoping_object_type != '#{joinable_type}' OR 
+           EXISTS (SELECT * FROM memberships WHERE memberships.joinable_type = '#{joinable_type}' AND memberships.joinable_id = feeds.scoping_object_id AND memberships.user_id = ?)", user.id)
+  }
   
   # The feed may have been delegated so inherit permissions from scoping_object
   # eg. a user (non-permissible) leaves a project, the permission to view the feed rests with the project because the feedable is the user itself
