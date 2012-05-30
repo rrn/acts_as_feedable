3d88e02249cc5183476a2dfb95cbd8b097778b0d
Ryan Wallace <rywall@gmail.com>
2012-05-30 16:06:24 -0700

    Add support for public feeds about closed joinables. Show feeds about unjoined joinables if they are about you.

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -7,14 +7,15 @@ class Feed < ActiveRecord::Base
   has_many :feed_aggregated_components, :dependent => :destroy, :order => 'feed_aggregated_components.updated_at DESC'
   
   # When the feedable has been deleted, inherit permissions from the scoping_object instead
-  acts_as_joinable_component :parent => 'permission_inheritance_target', :polymorphic => true
+  acts_as_joinable_component :parent => 'permission_inheritance_target', :polymorphic => true, :view_permission => lambda {|feed| feed.feedable.acts_like?(:joinable) ? :find : :view }
   
   default_scope order('feeds.updated_at DESC')
 
-  # Filter feeds about public joinables that you haven't joined
+  # Filter feeds about public joinables that you haven't joined, unless the feed is actually about you
   scope :without_unjoined, lambda {|joinable_type, user| 
-    where("feeds.scoping_object_type IS NULL OR 
-           feeds.scoping_object_type != '#{joinable_type}' OR 
+    where("feeds.scoping_object_type IS NULL OR
+           feeds.scoping_object_type != '#{joinable_type}' OR
+           (feeds.feedable_type = 'User' AND feeds.feedable_id = #{user.id}) OR
            EXISTS (SELECT * FROM memberships WHERE memberships.joinable_type = '#{joinable_type}' AND memberships.joinable_id = feeds.scoping_object_id AND memberships.user_id = ?)", user.id)
   }
   
