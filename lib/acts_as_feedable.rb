d47930074d3f9fe99dc1794494227fe44857f3c0
Ryan Wallace <rywall@gmail.com>
2012-06-06 10:19:56 -0700

    move feedable code related to joinable into this gem.

--- lib/acts_as_feedable.rb
+++ lib/acts_as_feedable.rb
@@ -4,6 +4,11 @@ module ActsAsFeedable
   class Engine < Rails::Engine
     initializer "acts_as_feedable.init" do
       ActiveRecord::Base.send :extend, Feedable::ActsAsFeedable::ActMethod
+
+      # Add feeds to joinable models
+      Membership.acts_as_feedable :parent => 'joinable', :delegate => {:references => 'user', :actions => {:created => 'joined', :destroyed => 'left'}}
+      MembershipInvitation.acts_as_feedable :parent => 'joinable', :delegate => {:references => 'user', :actions => {:created => 'invited', :destroyed => 'cancelled_invite'}}
+      MembershipRequest.acts_as_feedable :parent => 'joinable', :delegate => {:references => 'user', :actions => {:created => 'requested', :destroyed => 'cancelled_request'}}
     end    
   end
 end
